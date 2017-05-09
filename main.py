bfd787990bc65885d9c654d4a9b3de6de7eb095f
b0204888100 <b0204888100@gmail.com>
2017-05-10 01:46:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,14 @@ def hello():
 
 @app.route('/vm/start2')
 def start_vm():
+    credentials = AppAssertionCredentials(scope='https://www.googleapis.com/auth/compute')
+    http = credentials.authorize(httplib2.Http(memcache))
+    compute = discovery.build('compute', 'v1', http=http)
+    
+    # Start the VM!
     result = compute.instances().start(instance=INSTANCE_NAME, zone=INSTANCE_ZONE, project=PROJECT).execute()
+    logging.debug(result)
+    return json.dumps(result, indent=4)
     
 @app.errorhandler(404)
 def page_not_found(e):
