2892a164597ef5ce894df482e8a579f09d37184f
b0204888100 <b0204888100@gmail.com>
2017-05-10 00:10:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
+INSTANCE_NAME = 'instance-1'
+INSTANCE_ZONE = 'asia-east1-a'
+PROJECT = 'esoteric-cider-165206'
 
 
 @app.route('/')
@@ -11,7 +14,10 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
-
+@app.route('/vm/start2')
+def start_vm():
+    result = compute.instances().start(instance=INSTANCE_NAME, zone=INSTANCE_ZONE, project=PROJECT).execute()
+    
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
