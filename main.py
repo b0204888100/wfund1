fa43fe540c8cdf7ec9142aa2ab41f328cb7e409e
b0204888100 <b0204888100@gmail.com>
2017-05-19 19:13:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import json
 import httplib2
-import logging
-#from apiclient 
-import discovery
+import logging 
+from apiclient import discovery
 from google.appengine.api import memcache
 from oauth2client.contrib.appengine import AppAssertionCredentials
 
