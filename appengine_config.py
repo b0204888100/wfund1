8fbb0d0b43743c013c435d1bbd9d95c34ed61546
b0204888100 <b0204888100@gmail.com>
2017-05-19 16:59:30 +0800

    Update appengine_config.py

--- appengine_config.py
+++ appengine_config.py
@@ -1,9 +1,9 @@
 """`appengine_config` gets loaded when starting a new application instance."""
-#import vendor
+import vendor
 # insert `lib` as a site directory so our `main` module can load
 # third-party libraries, and override built-ins with newer
 # versions.
-#vendor.add('lib')
+vendor.add('lib')
 #vendor.add('libx')
 import sys
 import os.path
@@ -11,6 +11,6 @@ import os.path
 # third-party libraries.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 # appengine_config.py
-from google.appengine.ext import vendor
+#from google.appengine.ext import vendor
 # Add any libraries install in the "lib" folder.
-vendor.add('lib')
+#vendor.add('lib')
