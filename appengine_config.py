0d8d6360b77611449f4a3241456706aaf5d8cd61
b0204888100 <b0204888100@gmail.com>
2017-05-19 16:47:10 +0800

    Update appengine_config.py

--- appengine_config.py
+++ appengine_config.py
@@ -10,3 +10,7 @@ import os.path
 # add `lib` subdirectory to `sys.path`, so our `main` module can load
 # third-party libraries.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+# appengine_config.py
+from google.appengine.ext import vendor
+# Add any libraries install in the "lib" folder.
+vendor.add('lib')
